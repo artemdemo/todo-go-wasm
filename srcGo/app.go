a06db3607113c8aea1110b13970667650b0ab4a1
Artem Demo <artemdemo@gmail.com>
2019-10-01 20:55:07 +0300

    added form model

--- srcGo/app.go
+++ srcGo/app.go
@@ -13,6 +13,8 @@ var toDoList = models.ToDoList{
     Items: []models.ToDoItem{},
 }
 
+var form = models.Form{}
+
 func initToDoList() {
     toDoList.AddTodoItem("First title", false)
     toDoList.AddTodoItem("Second title", true)
@@ -71,29 +73,7 @@ func renderApp() {
         htmlrender.ElementDef{
             Tag: "div",
             Children: []htmlrender.ElementDef{
-                {
-                    Tag: "div",
-                    ClassName: "mb-4",
-                    Children: []htmlrender.ElementDef{
-                        {
-                            Tag: "input",
-                            ID: "todo-title",
-                            ClassName: "bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal mb-4",
-                            Attributes: []htmlrender.ElementAttr{
-                                {
-                                    Name: "placeholder",
-                                    Content: "Title",
-                                },
-                            },
-                        },
-                        {
-                            Tag: "button",
-                            ID: "submit-todo",
-                            ClassName: "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded",
-                            InnerText: "Add ToDo",
-                        },
-                    },
-                },
+                form.GetElementDef(),
                 {
                     Tag: "div",
                     ClassName: "todo-list mb-5",
---
+++ srcGo/models/form.go
@@ -0,0 +1,33 @@
+package models
+
+import (
+    "../htmlrender"
+)
+
+type Form struct {}
+
+func (form Form) GetElementDef() htmlrender.ElementDef {
+    return htmlrender.ElementDef{
+        Tag: "div",
+        ClassName: "mb-4",
+        Children: []htmlrender.ElementDef{
+            {
+                Tag: "input",
+                ID: "todo-title",
+                ClassName: "bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal mb-4",
+                Attributes: []htmlrender.ElementAttr{
+                    {
+                        Name: "placeholder",
+                        Content: "Title",
+                    },
+                },
+            },
+            {
+                Tag: "button",
+                ID: "submit-todo",
+                ClassName: "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded",
+                InnerText: "Add ToDo",
+            },
+        },
+    }
+}
