c226c2d8b2ce0dc5eca322b2b71f0b9536dc0720
Artem Demo <artemdemo@gmail.com>
2019-10-19 18:02:35 +0300

    updated test run

--- tests.sh
+++ tests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo "srcGo/services/classnames"
-go test srcGo/services/classnames_test.go srcGo/services/classnames.go -cover
+echo "srcGo/services/*"
+go test ./services -test.v -cover
